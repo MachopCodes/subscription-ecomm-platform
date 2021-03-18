539445cd1d18b8e31336f5ebd8a8dd5ddeb4cbe3
machopcodes <william.andreae@uconn.edu>
2021-03-18 11:41:59 -0400

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/Link'
+
+export default function Index() {
+    return (
+        <>
+            <Link href="/"><a>Home</a></Link>
+            <p>Hello, I am the about page</p>
+        </>
+    )
+}
