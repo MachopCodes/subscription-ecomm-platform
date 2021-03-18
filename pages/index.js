11405a472027350a6c2dd232f1dfde1fc2f35027
machopcodes <william.andreae@uconn.edu>
2021-03-18 16:56:34 -0400

    altered index

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,192 @@
-import Link from 'next/link'
-import fetch from 'isomorphic-unfetch'
+import Link from "next/link";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Container, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
-export default function Index({ shows }) {
-    return (
-        <>
-            <div>Hello there!</div>
-            <Link href="/about"><a>About</a></Link>
-            <h1>Series</h1>
-            <ul>
-                {shows && shows.map(show => 
-                    <li key={show.id}>
-                        <Link href="/show/[id]" as={`/show/${show.id}`}>
-                            <a>{show.name}</a>
-                        </Link>
-                    </li>)}
-            </ul>
-        </>
-    )
-}
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
-Index.getInitialProps = async function () {
-    const res = await fetch('https://api.tvmaze.com/search/shows?q=sherlock')
-    const data = await res.json()
-    const shows = {shows: data.map(entry => entry.show) }
-    console.log('Rendering ...', shows)
-    return shows
+export default function Index() {
+  const [cats, setCats] = useState("");
+  const apiUrl = "https://api.thecatapi.com/v1/images/search?mime_types=gif";
+  const apiKey = "a0769af0-040e-4860-855b-912d506455e6";
+  const classes = useStyles();
+
+  const catNames = [
+    "Bella",
+    "Kitty",
+    "Lily",
+    "Charlie",
+    "Lucy",
+    "Leo",
+    "Milo",
+    "Jack",
+    "Nala",
+    "Sam",
+    "Simba",
+    "Chloe",
+    "Baby",
+    "Sadie",
+    "Ziggy",
+    "Princess",
+    "Salem",
+    "Sophie",
+    "Shadow",
+    "Izzy",
+    "Cleo",
+    "Boots",
+    "Loki",
+    "Daisy",
+    "Cooper",
+    "Missy",
+    "Oreo",
+    "Tiger",
+    "Lulu",
+    "Tucker",
+    "Jasmine",
+    "Jackson",
+    "Murphy",
+    "Pepper",
+    "Fiona",
+    "Jax",
+    "Frank",
+    "Romeo",
+    "Millie",
+    "Abby",
+    "Minnie",
+    "Olivia",
+    "Lola",
+    "Athena",
+    "Teddy",
+    "Ruby",
+    "Oscar",
+    "Bear",
+    "Moose",
+    "Pumpkin",
+    "Willow",
+    "Mittens",
+    "Coco",
+    "Penny",
+    "Sammie",
+    "Theo",
+    "Kali",
+    "Bob",
+    "Clyde",
+    "Tigger",
+    "Buddy",
+    "Joey",
+    "Emma",
+    "Ollie",
+    "Toby",
+    "George",
+    "Marley",
+    "Bagheera",
+    "Belle",
+    "Binx",
+    "Boo",
+    "Ash",
+    "Scout",
+    "Gizmo",
+    "Louie",
+    "Ginger",
+    "Midnight",
+    "Mochi",
+    "Blu",
+    "Frankie",
+    "Rosie",
+    "Ella",
+    "Calvin",
+    "Lucky",
+    "Hazel",
+    "Thor",
+    "Gus",
+    "Maggie",
+    "Piper",
+    "Harley",
+    "Rocky",
+    "Peanut",
+    "Mimi",
+    "Kitten",
+    "Remi",
+    "Annie",
+    "Sunny",
+    "Layla",
+    "Riley",
+    "Walter"
+]
+const cost = Math.random()*100
+
+  useEffect(() => {
+    const fetchCats = async () => {
+      const res = await axios.get(
+        apiUrl + "&limit=10"
+      );
+      setCats(res.data);
+    };
+    fetchCats();
+  }, []);
+  cats && console.log("cats are: ", cats);
+  console.log('total catnames are: ', catNames[0])
+
+  // cats.map(cat => {
+  //     console.log(cat)
+  // })
+  return (
+    <>
+      <div>Hello there!</div>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <h1>Welcome to the Catporium!</h1>
+      <p>Select a cat you would like to buy!</p>
+      <Container>
+      <Grid alignItems="center" spacing={4} container>
+        {cats &&
+          cats.map((cat) => (
+            <Grid item xs={6} key={cat.id}>
+              <Card className={classes.root}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={cat.url}
+                    title={cat.id}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {catNames[(Math.floor(Math.random()*100))]}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >${((Math.random()*100) + 80).toFixed(2)}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Add to Cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+      </Grid>
+      </Container>
+    </>
+  );
 }
