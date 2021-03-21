bdf0e641773270d9f4ed3c0b3a11350c95ea22df
machopcodes <william.andreae@uconn.edu>
2021-03-21 00:48:58 -0400

    added security check

--- pages/api/subscription/[user].js
+++ pages/api/subscription/[user].js
@@ -1,5 +1,3 @@
-import subscription from ".";
-
 const { session } = require("next-auth/client");
 const useSubscription = require("../../../lib/mongoose/schemas/Subscription")
 const mongooseConnect = require("../../../lib/mongoose/mongooseConnection");
@@ -28,12 +26,21 @@ export default (req, res) => {
       .then((subscription) => res.status(201).json(subscription))
       .catch((err) => res.status(400).json({ message: err.message }));
   } else if (req.method === "GET") {
-    Subscription.findOne({ user: req.query.user})
-      .then(handle404)
-      .then((subscription) => {
-        return res.status(200).json(subscription);
-      })
-      .catch((err) => res.status(400).json({ message: err.message }));
+    if(req.query.user.length === 24) {
+      Subscription.findById(req.query.user)
+        .then(handle404)
+        .then((subscription) => {
+          return res.status(200).json(subscription);
+        })
+        .catch((err) => res.status(400).json({ message: err.message }))
+      } else {
+          Subscription.findOne({ user: req.query.user })
+            .then(handle404)
+            .then((subscription) => {
+              return res.status(200).json(subscription);
+            })
+            .catch((err) => res.status(400).json({ message: err.message }))
+          }
     } else if (req.method === "DELETE" && session) {
       Subscription.findOne({ user: req.query.user })
         .then(handle404)
