1f4ae384d70b3222960df466a02ffa217cd24b1c
machopcodes <william.andreae@uconn.edu>
2021-03-22 22:46:20 -0400

    refacgtored signin

--- Components/Header.js
+++ Components/Header.js
@@ -1,64 +1,35 @@
 import Link from 'next/link';
-import axios from 'axios'
-import { signin, signout, useSession } from 'next-auth/client';
-import React, { useState, useEffect } from 'react';
+import { signout, useSession } from 'next-auth/client';
+import React from 'react';
+import SignIn from './Signin'
+import SubscriptionModal from './SubscriptionModal'
 
-const Header = ({ user, setUser }) => {
+const Header = ({ user, subscription }) => {
   const [session, loading] = useSession();
-  const [subscription, setSubscription] = useState()
-
-  const fetchSubscriptions = async () => {
-    const result = await axios.get(`/api/subscription/${user}`);
-    setSubscription(result.data)
-  };
-
-  useEffect(() => session && setUser(session.user.email), [session])
-  useEffect(() => user && fetchSubscriptions(), [user])
-
   return (
     <header>
       <nav>
       <Link href="/">
           <a className="logo">
             <span style={{ color: '#f06292' }}>C</span>
-            <span style={{ color: '#29b6f6' }}>A</span>
-            <span style={{ color: '#8bc34a' }}>T</span>
             <span style={{ color: '#f06292' }}>P</span>
-            <span style={{ color: '#29b6f6' }}>O</span>
-            <span style={{ color: '#f06292' }}>R</span>
-            <span style={{ color: '#29b6f6' }}>I</span>
-            <span style={{ color: '#8bc34a' }}>U</span>
-            <span style={{ color: '#f06292' }}>M</span>
           </a>
         </Link>
         <p>
-          {!session && (
-            <a
-              href="/api/auth/signin"
-              onClick={(e) => {
-                e.preventDefault();
-                signin();
-              }}
-            >
-              <button className="signInButton">Sign in</button>
-            </a>
-          )}
+          {!session && <SignIn />}
           {session && (
             <>
-              <Link href="/profile">
-                <a>
-                  <span
-                    style={{ backgroundImage: `url(${session.user.image})` }}
-                    className="avatar"
-                  />
-                </a>
-              </Link>
-              <span className="email">Hi {session.user.name}!</span>
-              {subscription && (
-                <Link href={`/subscription/${subscription._id}`} >
-                  <button className="cartButton">My Subscription</button>
+              <span
+                style={{ backgroundImage: `url(${session.user.image})` }}
+                className="avatar"
+              />
+              {subscription 
+              ? (
+                <Link href={`/my-subscription`} >
+                  <button className="cartButton">
+                    {subscription.subscription} Plan</button>
                 </Link>
-              )}
+              ) : <SubscriptionModal user={user} />}
               <a
                 href="/api/auth/signout"
                 onClick={(e) => {
@@ -75,6 +46,11 @@ const Header = ({ user, setUser }) => {
       <style jsx>{`
         header {
           border-bottom: 1px solid #ccc;
+          background-color: white;
+          position: fixed;
+          top: 0;
+          width: 100%;
+          z-index: 5;
         }
         nav {
           display: flex;
@@ -97,22 +73,8 @@ const Header = ({ user, setUser }) => {
           background-size: cover;
           border: 2px solid #ddd;
         }
-        .email {
-          margin-right: 1rem;
-          margin-left: 0.25rem;
-          font-weight: 600;
-        }
-        .signInButton {
-          background-color: #1eb1fc;
-          color: #fff;
-          border: none;
-          border-radius: 4px;
-          cursor: pointer;
-          font-size: 1rem;
-          padding: 0.5rem 1rem;
-        }
         .signOutButton {
-            background-color: #8bc34a;
+            background-color: #333;
             color: #fff;
             border: none;
             border-radius: 4px;
@@ -129,15 +91,6 @@ const Header = ({ user, setUser }) => {
             font-size: 1rem;
             padding: 0.5rem 1rem;
           }
-        .signInButton {
-          background-color: #1eb1fc;
-        }
-        .signInButton:hover {
-          background-color: #1b9fe2;
-        }
-        .signOutButton {
-          background-color: #333;
-        }
         .signOutButton:hover {
           background-color: #555;
         }
